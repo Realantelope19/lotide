e970d6070c74fd1d3f2c49864fc4f092e177a856
Eniola Busari <eniola.busari@gmail.com>
2021-08-23 22:51:07 +0000

    middle

---
+++ test/middleTest.js
@@ -0,0 +1,7 @@
+const middle = require('../middle')
+
+const eqArrays = require('../eqArrays');
+const assertArraysEqual = require('../assertArraysEqual');
+
+console.log(middle([4,5,]))
+console.log(middle([1, 2, 3, 4, 5, 6]))
